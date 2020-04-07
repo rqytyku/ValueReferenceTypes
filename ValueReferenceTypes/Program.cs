777fdd0c50def8bccde0b2b201d95fe54ad4df78
Romela Qytyku <rqytyku2@uet.edu.al>
2020-04-07 12:09:15 +0200

    Add project files.

---
+++ ValueReferenceTypes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueReferenceTypes
+{
+    class ReferenceValue
+     {
+        public int vlere;
+     }
+
+    class Shembull
+    {
+        static void Llogarit(ReferenceValue a, ReferenceValue b)
+        {
+            a.vlere = a.vlere * a.vlere;
+            b.vlere = b.vlere * b.vlere;
+            Console.WriteLine(a.vlere + " " + b.vlere);
+        }
+        static void Main(string[] args)
+        {
+            ReferenceValue p1 = new ReferenceValue();
+            ReferenceValue p2 = new ReferenceValue();
+            p1.vlere = 5;
+            p2.vlere = 10;
+            Console.WriteLine("Vlerat para metodes");
+            Console.WriteLine(p1.vlere + " " + p2.vlere);
+            Console.WriteLine("Vlerat pasi thirret metoda");
+            Llogarit(p1, p2);
+            Console.WriteLine("Vlerat finale");
+            Console.WriteLine(p1.vlere + " " + p2.vlere);
+  
+            Console.ReadLine();
+        }
+    }
+}
